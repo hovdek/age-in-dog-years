1da21a5361d269be648868c31c286f33aa09345d
hovdek <kaitlynhovde@gmail.com>
2022-06-10 00:21:31 -0500

    added js code
    practicing with js. refer to js comments on current plan/thought process

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+
 // my current age
-const myAge = 24;
+var myAge = document.getElementById("age-textarea").value;
 
 // variable that will be able to be changed
 let earlyYears = 2;
@@ -18,7 +19,26 @@ console.log(laterYears);
 let myAgeInDogYears = earlyYears + laterYears;
 
 // setting variable with my name
-let myName = 'Kaitlyn Hovde'.toLowerCase();
+// let myName = ' '.toLowerCase();
+
+// creating the text on screen to tell you how old you are in dog years
+
+// document.createTextNode();
 
 // printing the statement of my name, my age, and how old I am in dog years
-console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+console.log(`I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+// this is currently turning the submit button into the statement I am trying to get to appear below the submit button. Ideally we would be able to use the submit button multiple times. 
+const targetDiv = document.getElementById("third");
+const btn = document.getElementById("toggle");
+
+btn.onclick = function () {
+    if (targetDiv.style.display == "none") {
+        targetDiv.style.display = "block"; 
+    } else {
+        targetDiv.style.display = "none";
+    }
+    console.log(`I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+    document.getElementById("toggle").innerHTML = `I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`;
+};
+
